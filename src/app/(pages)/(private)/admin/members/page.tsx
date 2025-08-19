6bf2f8a59df7886b9bf473fc16fd79ee75083b21
LeviTorresx <levist42@gmail.com>
2025-08-18 19:28:56 -0500

    Refactor AdminMembersDashboard for improved readability and consistency

--- src/app/(pages)/(private)/admin/members/page.tsx
+++ src/app/(pages)/(private)/admin/members/page.tsx
@@ -1,32 +1,30 @@
-'use client';
-import MemberTable from '@/app/components/member/MemberTable'
-import { selectMembers } from '@/app/store/selectors/memberSelectors'
-import React from 'react'
-import { useSelector } from 'react-redux'
+"use client";
+import MemberTable from "@/app/components/member/MemberTable";
+import { selectMembers } from "@/app/store/selectors/memberSelectors";
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function AdminMembersDashboard() {
+  const members = useSelector(selectMembers);
 
-    const members = useSelector(selectMembers)
-
-    const validMembers = members.filter((m)=> m.valid)
-    const invalidMembers= members.filter((m)=> !m.valid)
-
+  const validMembers = members.filter((m) => m.valid);
+  const invalidMembers = members.filter((m) => !m.valid);
 
   return (
-   <div className="p-6">
-      <MemberTable 
-        members={validMembers} 
-        title="Miembros por validar" 
-        color="yellow" 
-        validState={false} 
+    <div className="p-6">
+      <MemberTable
+        members={validMembers}
+        title="Miembros por validar"
+        color="yellow"
+        validState={false}
       />
-      
-      <MemberTable 
-        members={invalidMembers} 
-        title="Miembros validados" 
-        color="green" 
-        validState={true} 
+
+      <MemberTable
+        members={invalidMembers}
+        title="Miembros validados"
+        color="green"
+        validState={true}
       />
     </div>
-  )
+  );
 }
