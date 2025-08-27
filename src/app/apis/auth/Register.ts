3734a8b9e33edee92b1b55ce4e5b2935cb089ceb
LeviTorresx <levist42@gmail.com>
2025-08-27 18:23:49 -0500

    Rename param name for register fuction

--- src/app/apis/auth/Register.ts
+++ src/app/apis/auth/Register.ts
@@ -1,11 +1,9 @@
 import { UserRequest } from "@/app/types/User";
 import axios from "axios";
 
-export async function register(User: UserRequest) {
+export async function register(userRequest: UserRequest) {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const response = await axios.post(`${apiUrl}/auth/register`, User, {
-    withCredentials: true,
-  });
+  const response = await axios.post(`${apiUrl}/auth/register`, userRequest,);
 
   if (response.status === 200) {
     return await response.data;
