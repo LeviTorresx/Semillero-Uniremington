8bce7cd2a3691829684ba2bc0fb238ff1027c6d7
LeviTorresx <levist42@gmail.com>
2025-08-29 09:26:18 -0500

    Refactor data providers: add member fetching logic to PrivateDataProvider and PublicDataProvider, ensuring members are loaded alongside projects.

--- src/app/(pages)/(private)/member/PrivateMemberProvider.tsx
+++ src/app/(pages)/(private)/member/PrivateMemberProvider.tsx
@@ -6,6 +6,7 @@ import { AppDispatch, RootState } from "@/app/store/store";
 import { fetchUserThunk } from "@/app/store/features/AuthSlice";
 import { fetchProjects } from "@/app/store/thunks/projectsThunks";
 import { fetchNews } from "@/app/store/thunks/NewsThunks";
+import { fetchMembers } from "@/app/store/thunks/MembersThunks";
 
 export default function PrivateDataProvider({
   children,
@@ -17,18 +18,21 @@ export default function PrivateDataProvider({
   const projects = useSelector((state: RootState) => state.projects.projects);
   const userAuth = useSelector((state: RootState) => state.auth);
   const news =  useSelector((state: RootState) => state.news.news);
+  const members =  useSelector((state: RootState) => state.members.users);
 
   useEffect(() => {
     const needsUser = !userAuth || !userAuth.isAuthenticated;
     const needsProjects = !projects || projects.length === 0;
     const needsNews = !news || news.length === 0;
+    const needsMembers = !members || members.length === 0;
 
-    if (needsUser || needsProjects) {
+    if (needsUser || needsProjects || needsMembers) {
       if (needsUser) dispatch(fetchUserThunk());
       if (needsProjects) dispatch(fetchProjects());
       if (needsNews) dispatch(fetchNews());
+      if (needsMembers) dispatch(fetchMembers());
     }
-  }, [dispatch, userAuth, projects, news]);
+  }, [dispatch, userAuth, projects, news, members]);
 
   return <>{children}</>;
 }
--- src/app/(pages)/(public)/PublicDataProvider.tsx
+++ src/app/(pages)/(public)/PublicDataProvider.tsx
@@ -1,21 +1,26 @@
-
-'use client';
+"use client";
 
 import { AppDispatch, RootState } from "@/app/store/store";
+import { fetchMembers } from "@/app/store/thunks/MembersThunks";
 import { fetchProjects } from "@/app/store/thunks/projectsThunks";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-export default function PublicDataProvider({ children }: { children: React.ReactNode }) {
+export default function PublicDataProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const dispatch = useDispatch<AppDispatch>();
   const projects = useSelector((state: RootState) => state.projects.projects);
+  const members = useSelector((state: RootState) => state.members.users);
 
   useEffect(() => {
-    if (!projects) {
+    if (!projects || !members) {
       dispatch(fetchProjects());
+      dispatch(fetchMembers());
     }
-  }, [dispatch, projects]);
+  }, [dispatch, projects, members]);
 
   return <>{children}</>;
 }
-
